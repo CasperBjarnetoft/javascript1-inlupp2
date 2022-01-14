c132ba0d306e74d2ed959120d2c9bc8133f68815
Casper Bjarnetoft <casper.ason@hotmail.se>
2022-01-14 18:07:23 +0100

    lagt till kod

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+const form = document.querySelector('#Form')
+const headline = document.querySelector('#headline')
+const about = document.querySelector('#about')
 const output = document.querySelector('#output')
-const checkbox = document.querySelector('#tac')
+let tac = document.querySelector('#tac')
 
 let todos = [];
+let number = 1;
 
 const getjson = (url) => {
     fetch(url)
     .then((response) => response.json())
     .then((json) =>  {
         todos = json;
-        console.log(json[9])
         
         output.innerHTML = ''
         todos.forEach(todo => {
@@ -16,18 +19,17 @@ const getjson = (url) => {
                 output.innerHTML += `
                 <div id="todon" class="person d-flex is-notdone">
                 <div class="output">
+                <input type="checkbox" id="tac" class="tac-control">
                 <div class="text-group">
-                <p id="output-text">${todo.id}</p>
+                <p id="output-text">${number}</p>
                 <p id="output-text">${todo.title}</p>
                 <p id="output-text">${todo.body}</p>
                 </div>
-                <div class="output-group d-flex">
-                    <input type="checkbox" id="tac" class="tac-control">
-                    <button id="resetbutton" class="btndelete">X</button>
-                </div>
+                <button type="button" id="resetbutton" class="btndelete">X</button>
                 </div>
                 </div>
                 `
+                number++;
             }
 
         })        
@@ -35,4 +37,83 @@ const getjson = (url) => {
 }
 
 getjson('https://jsonplaceholder.typicode.com/posts/');
+
+const validateText = (id) => {
+    let input = document.querySelector(id)
+
+    if(input.value === '' || input.value.length < 2) {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid')
+        input.focus();
+        return false;
+    }
+    else {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        return true;
+    }
+}
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const errors = [];
+
+    for (let i = 0; i< e.currentTarget.length; i++) {
+        if (e.currentTarget[i].type === 'text') {
+            errors[i] = validateText('#' + e.currentTarget[i].id)
+            console.log(i)
+        }
+    }
+
+
+    if(errors.includes(false)) {
+
+    }
+    else {
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: headline.value,
+                body: about.value,
+                useriId: 1,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            todos.push(json)
+            headline.value = ''
+            about.value = ''
+            console.log(todos)
+
+            let numbers = 1;
+            output.innerHTML = ''
+            todos.forEach(todo => {
+                    output.innerHTML += `
+                    <div id="todon" class="person d-flex is-notdone">
+                    <div class="output">
+                    <input type="checkbox" id="tac" class="tac-control">
+                    <div class="text-group">
+                    <p id="output-text">${numbers}</p>
+                    <p id="output-text">${todo.title}</p>
+                    <p id="output-text">${todo.body}</p>
+                    </div>
+                    <button type="button" id="resetbutton" class="btndelete">X</button>
+                    </div>
+                    </div>
+                    `
+                    todo.id++;
+                    numbers++;
+                })
+
+            })
+    }    
+})
+
+
+
 
